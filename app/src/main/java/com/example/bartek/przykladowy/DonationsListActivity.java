4265f6a8647c591eee736bbffd08ca0f3deb2dce
Bartosz Głowinkowski <bg311655@students.mimuw.edu.pl>
2016-08-30 16:01:31 +0200

    Dodanie drugiego activity, do które ma wyświetlać w przyszłości listę donacji.

--- app/src/main/java/com/example/bartek/przykladowy/DonationsListActivity.java
+++ app/src/main/java/com/example/bartek/przykladowy/DonationsListActivity.java
@@ -2,18 +2,12 @@ package com.example.bartek.przykladowy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class DonationsListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void onClick_showDonations(View v) {
         setContentView(R.layout.activity_donations_list);
     }
-
 }
