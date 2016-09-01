dd36aceff641b933b08f7d3a5263ee2d09eea79a
Bartosz Głowinkowski <bg311655@students.mimuw.edu.pl>
2016-09-01 19:13:45 +0200

    Wykorzystanie aktywności donationsList. Poprawa layoutu MainActivity (pozostał do zrobienia tylko manage users).

--- app/src/main/java/com/example/bartek/przykladowy/DonationsListActivity.java
+++ app/src/main/java/com/example/bartek/przykladowy/DonationsListActivity.java
@@ -2,6 +2,7 @@ package com.example.bartek.przykladowy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DonationsListActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class DonationsListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donations_list);
+    }
+
+    public void onClick_endThisActivity(View v) {
+        finish();
     }
 }
